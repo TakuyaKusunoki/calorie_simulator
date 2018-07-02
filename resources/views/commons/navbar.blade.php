a1e0e131eae7222e64c66137b97d01dddaaa11f0
takuya.kusunoki <takuya.kusunoki@softfront-japan.co.jp>
2018-07-02 07:26:16 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,12 +8,29 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Microposts</a>
+                <a class="navbar-brand" href="/">Calorie Simulator</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Signup</a></li>
-                    <li><a href="#">Login</a></li>
+                    @if (Auth::check())
+                        @if (Auth::user()->type = "user")
+                            <li><a href="#">食事内容登録</a></li>
+                            <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">My profile</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                            </li>
+                        @else
+                            <li><a href="#">Users</a></li>
+                            <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        @endif
+                    @else
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li><a href="#">Login</a></li>
+                    @endif
                 </ul>
             </div>
         </div>
